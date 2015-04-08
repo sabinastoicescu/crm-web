f161a1dfaad9b199bcf14de73e84e1225288a8bd
Sabina <sabina_stoicescu@yahoo.com>
2015-04-08 16:18:15 -0400

    added last changes

--- crm2.rb
+++ crm2.rb
@@ -23,3 +23,9 @@ get '/contacts/new' do
 	erb :new_contact
 end
 
+post '/contacts' do
+	new_contact=Contact.new(params[:first_name], params[:last_name], params[:email], params[:notes])
+	$rolodex.add_contact(new_contact)
+redirect to ('/contacts')
+end
+
